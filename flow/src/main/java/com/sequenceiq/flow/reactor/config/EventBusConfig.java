a9187fa8a59accffbeb486f15d4a751e71a474b0
sodre90 <perdos@cloudera.com>
2019-12-16 08:43:38 +0100

    CB-4778 handle every flow exception, termination retry allowed after 60 mins

--- flow/src/main/java/com/sequenceiq/flow/reactor/config/EventBusConfig.java
+++ flow/src/main/java/com/sequenceiq/flow/reactor/config/EventBusConfig.java
@@ -3,19 +3,26 @@ package com.sequenceiq.flow.reactor.config;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
 
+import javax.inject.Inject;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 
 import com.sequenceiq.cloudbreak.logger.concurrent.MDCCleanerThreadPoolExecutor;
+import com.sequenceiq.flow.core.ApplicationFlowInformation;
 import com.sequenceiq.flow.reactor.handler.ConsumerNotFoundHandler;
+import com.sequenceiq.flow.service.flowlog.FlowLogDBService;
 
 import reactor.Environment;
+import reactor.bus.Event;
 import reactor.bus.EventBus;
 import reactor.bus.spec.EventBusSpec;
 import reactor.core.dispatch.ThreadPoolExecutorDispatcher;
+import reactor.core.support.Exceptions;
 import reactor.core.support.NamedDaemonThreadFactory;
 import reactor.fn.timer.Timer;
 
@@ -33,6 +40,14 @@ public class EventBusConfig {
     @Value("${cb.eventbus.threadpool.backlog.size:1000}")
     private int eventBusThreadPoolBacklogSize;
 
+    @Inject
+    @Lazy
+    private ApplicationFlowInformation applicationFlowInformation;
+
+    @Inject
+    @Lazy
+    private FlowLogDBService flowLogDBService;
+
     @Bean
     public Timer timer(Environment env) {
         return env.getTimer();
@@ -49,6 +64,22 @@ public class EventBusConfig {
                 .env(env)
                 .dispatcher(new ThreadPoolExecutorDispatcher(eventBusThreadPoolBacklogSize, eventBusThreadPoolCoreSize, threadPoolExecutor))
                 .traceEventPath()
+                .dispatchErrorHandler(throwable -> {
+                    if (throwable.getCause() instanceof Exceptions.ValueCause) {
+                        if (((Exceptions.ValueCause) throwable.getCause()).getValue() instanceof Event) {
+                            Event event = (Event) ((Exceptions.ValueCause) throwable.getCause()).getValue();
+                            if (event.getHeaders() != null && event.getHeaders().get("FLOW_ID") != null) {
+                                String flowId = event.getHeaders().get("FLOW_ID").toString();
+                                LOGGER.error("Unhandled exception happened in flow {}, lets cancel it", flowId, throwable);
+                                flowLogDBService.getLastFlowLog(flowId).ifPresent(flowLog -> {
+                                    flowLogDBService.updateLastFlowLogStatus(flowLog, true);
+                                    applicationFlowInformation.handleFlowFail(flowLog);
+                                });
+                            }
+                        }
+                    }
+                    LOGGER.error("Exception happened in dispatcher", throwable);
+                })
                 .consumerNotFoundHandler(new ConsumerNotFoundHandler())
                 .get();
     }
